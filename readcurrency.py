657eba4249633396eb275ca9471b72047e2a9d6a
Serdar Durbayev <durbayevserdar@hotmail.com>
2018-11-13 12:50:41 -0500

    fix bugs

--- readcurrency.py
+++ readcurrency.py
@@ -1,15 +1,11 @@
 def readcurrency(filename):
     currency = {}
-    list1 = []
+    list1, list2 = [], []
     with open(filename , "r") as file_object:
         for line in file_object:
             list1 = line.split()
-            currency.update({"symbol": list1[0] , "rate": float(list1[1])})
-            print(currency)
-           # for element in line:
-
-            #print(line, end = "")
-
-
+            currency = {"symbol": list1[0] , "rate": float(list1[1])}
+            list2.append(currency)
+        print(*list2, sep = "\n")
 
 readcurrency("currency.txt")
